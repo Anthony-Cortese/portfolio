c522f81a946b8ca77fe5ffda9dd35b03f8490633
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-10-11 15:48:13 -0600

    maybe

--- cortese-portfolio/src/Navbar.js
+++ cortese-portfolio/src/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
